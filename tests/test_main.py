923f75440a02f91b4913f8d707139dc7df6b2f5b
Jorge Tilleria <jltilleriam@gmail.com>
2025-01-28 21:25:49 -0300

    ✅ refactor(tests): rename test-app.py to test_main.py and fix import path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,13 @@
 from fastapi.testclient import TestClient
+
+#### SOLVE PATH PROBLEM ####
+import sys
+import os
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),"src")
+sys.path.append(path)
 from main import app
+############################
+
 
 client = TestClient(app)
 
